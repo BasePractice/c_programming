8df7254ed5df632d6376270c0ee0dbe785c286bc
pastor <viruszold@gmail.com>
2018-11-16 11:40:22 +0300

    10.Multithreading - шаблон монитора

---
+++ _2.Practice/10.Synchrinized/monitor_impl.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <stdbool.h>
+#include <errno.h>
+
+#if defined(_WIN32)
+#  ifndef WIN32_LEAN_AND_MEAN
+#  define WIN32_LEAN_AND_MEAN
+#  endif
+
+#include <windows.h>
+typedef CRITICAL_SECTION Type;
+#else
+
+#include <unistd.h>
+#include <pthread.h>
+
+typedef pthread_mutex_t Type;
+#endif
+
+typedef int Object;
+
+typedef void (*FUNCTION_MONITOR_ENTER)(Object object);
+
+struct Monitor {
+    Object object;
+    Type synchronize_object;
+};
+
+void monitor_init(struct Monitor *monitor, Object object);
+
+void monitor_destroy(struct Monitor *monitor);
+
+void monitor_enter(struct Monitor *monitor, FUNCTION_MONITOR_ENTER cb);
+
+
+int main(int argc, char **argv) {
+    return EXIT_SUCCESS;
+}
+
+void monitor_init(struct Monitor *monitor, Object object) {
+    monitor->object = object;
+#if defined(_WIN32)
+#error "Not implement yet"
+#else
+    pthread_mutex_init(&monitor->synchronize_object, 0);
+#endif
+}
+
+void monitor_destroy(struct Monitor *monitor) {
+#if defined(_WIN32)
+#error "Not implement yet"
+#else
+    pthread_mutex_destroy(&monitor->synchronize_object);
+#endif
+}
