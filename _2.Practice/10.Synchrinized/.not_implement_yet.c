34dcac4e6f7de8a27bb457826b212b9d8e1068d9
pastor <viruszold@gmail.com>
2019-09-06 17:14:59 +0300

    Исправление ошибок для сборки в MacOS

---
+++ _2.Practice/10.Synchrinized/.not_implement_yet.c
@@ -0,0 +1,13 @@
+#include "monitor.h"
+
+void monitor_init(struct Monitor *monitor, void *object) {
+
+}
+
+void monitor_destroy(struct Monitor *monitor) {
+
+}
+
+void monitor_enter(struct Monitor *monitor, FUNCTION_MONITOR_ENTER cb) {
+
+}
