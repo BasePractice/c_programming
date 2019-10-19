a34ee7e1aba8cecbacf7b7ddd178933a2991b7aa
pastor <viruszold@gmail.com>
2019-10-20 00:53:23 +0300

    OPC UA подготовка сервера

---
+++ _4.OPCUA/main.c
@@ -0,0 +1,24 @@
+#include <signal.h>
+#include <stdlib.h>
+#include "open62541.h"
+
+UA_Boolean running = true;
+static void stop_handler(int signal) {
+    running = false;
+}
+
+int main(int argc, char **argv) {
+    UA_StatusCode ret;
+    UA_Server    *server;
+
+    signal(SIGINT,  stop_handler);
+    signal(SIGTERM, stop_handler);
+
+    server = UA_Server_new();
+    UA_ServerConfig_setDefault(UA_Server_getConfig(server));
+
+    ret = UA_Server_run(server, &running);
+
+    UA_Server_delete(server);
+    return ret == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
+}
